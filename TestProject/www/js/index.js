3f9be0f9e6291757c8f0312d970c3ccf3dc21b6c
Italo Gazzaneo <igazzaneo@gmail.com>
2019-01-05 16:52:23 +0100

    aaaa

--- TestProject/www/js/index.js
+++ TestProject/www/js/index.js
@@ -30,3 +30,49 @@ var app = {
 };
 
 app.initialize();
+
+//Create new table
+$("#createTable").click(function(){
+    alert("Clicked!!!!.");
+    onDeviceReady();
+    myDB.transaction(function(transaction) {
+    transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
+        function(tx, result) {
+            alert("Table created successfully");
+        },
+        function(error) {
+              alert("Error occurred while creating the table.");
+        });
+    });
+});
+
+//Insert New Data
+$("#insert").click(function(){
+  var title=$("#title").val();
+  var desc=$("#desc").val();
+  console.log(title +""+ desc);
+  myDB.transaction(function(transaction) {
+        var executeQuery = "INSERT INTO phonegap_pro (title, desc) VALUES (?,?)";
+        transaction.executeSql(executeQuery, [title,desc]
+            , function(tx, result) {
+                 alert('Inserted');
+            },
+            function(error){
+                 alert('Error occurred');
+            });
+    });
+});
+
+//Display Table Data
+$("#showTable").click(function(){
+  $("#TableData").html("");
+  myDB.transaction(function(transaction) {
+  transaction.executeSql('SELECT * FROM phonegap_pro', [], function (tx, results) {
+       var len = results.rows.length, i;
+       $("#rowCount").html(len);
+       for (i = 0; i < len; i++){
+          $("#TableData").append("<tr><td>"+results.rows.item(i).id+"</td><td>"+results.rows.item(i).title+"</td><td>"+results.rows.item(i).desc+"</td><td><a href='edit.html?id="+results.rows.item(i).id+"&title="+results.rows.item(i).title+"&desc="+results.rows.item(i).desc+"'>Edit</a> &nbsp;&nbsp; <a class='delete' href='#' id='"+results.rows.item(i).id+"'>Delete</a></td></tr>");
+       }
+    }, null);
+  });
+});
