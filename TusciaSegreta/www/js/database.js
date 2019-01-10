87174a22d36150a41f719afbb4c6960e75aafb3b
Italo Gazzaneo <igazzaneo@gmail.com>
2019-01-10 19:31:17 +0100

    Deploy test DB

--- TusciaSegreta/www/js/database.js
+++ TusciaSegreta/www/js/database.js
@@ -1,25 +1,90 @@
-
 var rowCount = 0;
+var database = null;
+var databaseUtente = null;
+
+var elencoSiti = new Array();
 
 function initDatabase() {
 
-  database = sqlitePlugin.openDatabase({name: 'tusciasegreta.db', location: 'default'});
+  window.resolveLocalFileSystemURL(cordova.file.dataDirectory + "/copied_tusciasegreta.db", openDb, setupDB);
 
-  database.transaction(function(transaction) {
-    transaction.executeSql('CREATE TABLE utente (id, nome_utente, email, password, cellulare, cittadinanza, lingua, cognome, nome)');
+}
+
+// copy a database file from www/ in the app directory to the data directory
+function copyDatabaseFile(dbName) {
+
+  var sourceFileName = cordova.file.applicationDirectory + 'www/' + dbName;
+  var targetDirName = cordova.file.dataDirectory;
+
+  return Promise.all([
+    new Promise(function (resolve, reject) {
+      resolveLocalFileSystemURL(sourceFileName, resolve, reject);
+    }),
+    new Promise(function (resolve, reject) {
+      resolveLocalFileSystemURL(targetDirName, resolve, reject);
+    })
+  ]).then(function (files) {
+    var sourceFile = files[0];
+    var targetDir = files[1];
+    return new Promise(function (resolve, reject) {
+      targetDir.getFile(dbName, {}, resolve, reject);
+    }).then(function () {
+      showMessage("Database già presente");
+    }).catch(function () {
+      //showMessage("file doesn't exist, copying it");
+      return new Promise(function (resolve, reject) {
+        sourceFile.copyTo(targetDir, 'copied_' + dbName, resolve, reject);
+      }).then(function () {
+        showMessage("Database copiato");
+      });
+    });
   });
+}
+// copy DB and open it
+function setupDB() {
+    showMessage("onSetupDB()");
+    copyDatabaseFile('tusciasegreta.db').then(function () {
+      database = sqlitePlugin.openDatabase({name: 'copied_tusciasegreta.db'});
+    }).catch(function (err) {
+      // error! :(
+      showMessage(err);
+    });
+
+}
+
+// db already in dir, this function only open it
+function openDb() {
+  showMessage("onOpenDb()");
+  database = sqlitePlugin.openDatabase({name: 'copied_tusciasegreta.db'});
+
+  /*database.transaction(function(transaction) {
+    transaction.executeSql('SELECT * FROM sito', [], function(ignored, resultSet) {
+      showMessage('Denominazione: ' + resultSet.rows.item(0).denominazione + ' - Video: ' + resultSet.rows.item(0).video + ' - Coordinate: ' + resultSet.rows.item(0).latitudine + " - " + + resultSet.rows.item(0).longitudine);
+    });
+  }, function(error) {
+    showMessage('SELECT error: ' + error.message);
+  });*/
 }
 
 function registrazioneDaApp() {
 
-  var email = $("#email").val();
-  var nome_utente = $("#username").val();
-  var cellulare = $("#cellulare").val();
-  var password = $("#password").val();
+  var form = $("#loginForm");
+	$("#submitButton",form).attr("disabled","disabled");
 
-  showMessage(email + " - " + nome_utente + " - " + cellulare + " - " + password);
+  var email       = $("#email", form).val();
+  var nome_utente = $("#username", form).val();
+  var cellulare   = $("#cellulare", form).val();
+  var password    = $("#password", form).val();
 
-  registraUtente(nome_utente, email, password, cellulare)
+  if(email != "" && nome_utente != "" && password != '' && cellulare != '')
+    registraUtente(nome_utente, email, password, cellulare);
+  else {
+    showMessage("Tutti i campi sono obbligatori");
+    $("#submitButton").removeAttr("disabled");
+  }
+
+  return false;
+
 }
 
 function registraUtente(nome_utente, email, password, cellulare) {
@@ -27,27 +92,28 @@ function registraUtente(nome_utente, email, password, cellulare) {
   // Effettuo la cancellazione preventiva dei record per evitare di avere più di un utente nel DB locale
   database.transaction(function(transaction) {
     transaction.executeSql('DELETE FROM utente', []);
-  }, function(error) {
-    //showMessage('Errore nella registrazione: ' + error.message);
-  }, function() {
-    //showMessage('Inserimento avvenuto correttamente.');
-  });
+    transaction.executeSql('INSERT INTO utente VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [1, nome_utente, 'Gazzaneo', 'Italo', email, password, '', cellulare, '']);
+    transaction.executeSql('select count(*) as recordCount from utente', [}, function(ignored, resultSet) {
+     showMessage("Siti trovati: " + resultSet.rows.item[0].recordCount)
+    };
 
-  database.transaction(function(transaction) {
-    transaction.executeSql('INSERT INTO utente VALUES (?, ?, ?, ?, ?)', [1, nome_utente, email, password, cellulare]);
   }, function(error) {
-    showMessage('Errore nella registrazione: ' + error.message);
+    showMessage('Errore nella cancellazione: ' + error.message);
   }, function() {
     showMessage('Inserimento avvenuto correttamente.');
+    fn.gotoPage('accesso.html');
   });
+
 }
 
 function logOut() {
 
   removeFromLocalStorage("loggedUser");
 
-  if(getValueFromLocalStorage('loggedUser') == 0)
+  if(getValueFromLocalStorage('loggedUser') == 0) {
     showMessage('Logout avvenuto con successo');
+    fn.gotoPage('accesso.html');
+  }
 
 }
 
@@ -69,19 +135,27 @@ function removeFromLocalStorage(key) {
 
 function accessoDaApp() {
 
-  var username = $("#username").val();
-  var password = $("#password").val();
+  var form = $("#loginForm");
+	$("#submitButton",form).attr("disabled","disabled");
 
-  showMessage(username + " - " + password);
+	var username = $("#username", form).val();
+	var password = $("#password", form).val();
+
+  //showMessage(username + " - " + password);
 
   if(username != "" && password != "")
     logIn(username, password);
   else if(username == "") {
     showMessage("Nome utente obbligatorio");
+    $("#submitButton").removeAttr("disabled");
   } else if(password == "") {
     showMessage("Password obbligatoria");
+    $("#submitButton").removeAttr("disabled");
   }
+
+  return false;
 }
+
 
 function logIn(login, password) {
 
@@ -98,14 +172,9 @@ function logIn(login, password) {
         // Utente presente e credenziali ok
         showMessage("Benvenuto: " + resultSet.rows.item[0].nome_utente + " - " + resultSet.rows.item[0].email + " - " + resultSet.rows.item[0].password + " - " + resultSet.rows.item[0].cellulare);
 
-        // Aggiorno il campo logged
-        /*database.transaction(function(transaction) {
-          transaction.executeSql('update utente set logged=1');
-        }, function(error) {
-          showMessage('UPDATE error: ' + error.message);
-        }, function() {
-          showMessage('UPDATE OK');
-        });*/
+        saveOnLocalStorage("loggedUser", "1");
+        showMessage('Login avvenuto con successo');
+        fn.gotoPage("accesso_effettuato.html");
 
       } else {
         showMessage('Nessun utente trovato!!!');
@@ -119,7 +188,7 @@ function logIn(login, password) {
   }, function(error) {
     showMessage('LOGIN error: ' + error.message);
   }, function() {
-    saveOnLocalStorage("loggedUser", "1");
+
   });
 
 }
@@ -139,13 +208,45 @@ function checkUser() {
   if(logged == 1) {
     fn.load('accesso_effettuato.html')
   } else {
-    showMessage('Utente non loggato');
+    //showMessage('Utente non loggato');
   }
 
+}
+
+function getElencoSiti() {
+
+  showMessage("onGetElencoSiti()");
+  var elenco = Array();
+
+  database.transaction(function(transaction) {
+    transaction.executeSql('SELECT * FROM sito', [], function(ignored, resultSet) {
+      showMessage("Siti trovati: " + resultSet.rows.length)
+
+      for(var x = 0; x < resultSet.rows.length; x++) {
+
+        var riga = new Array();
+        riga[0] = resultSet.rows.item[x].id;
+        riga[1] = resultSet.rows.item[x].denominazione;
+        riga[2] = resultSet.rows.item[x].descrizione;
+        riga[3] = resultSet.rows.item[x].video;
+        riga[4] = resultSet.rows.item[x].latitudine;
+        riga[5] = resultSet.rows.item[x].longitudine;
+
+        elenco[x] = riga;
+      }
+      return elenco;
+      //showMessage('Denominazione: ' + resultSet.rows.item(0).denominazione + ' - Video: ' + resultSet.rows.item(0).video + ' - Coordinate: ' + resultSet.rows.item(0).latitudine + " - " + + resultSet.rows.item(0).longitudine);
+    });
+  }, function(error) {
+    showMessage('SELECT error: ' + error.message);
+  });
+
+  //return elenco;
 }
 
 document.addEventListener('deviceready', function() {
 
   initDatabase();
+  elencoSiti = getElencoSiti();
 
 });
